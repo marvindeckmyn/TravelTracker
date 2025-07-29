49ec6d6b02874212c23758b5a58a7e84deb2fb6f
marvindeckmyn <marvindeckmyn@gmail.com>
2025-07-29 20:37:11 +0200

    Use EF Core and postgresql

--- TravelTracker.Api/Controllers/VisitsController.cs
+++ TravelTracker.Api/Controllers/VisitsController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TravelTracker.Api.Data;
 using TravelTracker.Api.Models;
 
@@ -8,66 +9,85 @@ namespace TravelTracker.Api.Controllers
     [ApiController]
     public class VisitsController : ControllerBase
     {
+        private readonly DataContext _context;
+
+        public VisitsController(DataContext context)
+        {
+            _context = context;
+        }
+        
         // GET: api/Visits
         [HttpGet]
-        public ActionResult<IEnumerable<Visit>> GetVisits()
+        public async Task<ActionResult<IEnumerable<Visit>>> GetVisits()
         {
-            return Ok(VisitStore.Visits);
+            return await _context.Visits.ToListAsync();
         }
         
         // GET: api/Visits/5
         [HttpGet("{id}")]
-        public ActionResult<Visit> GetVisit(int id)
+        public async Task<ActionResult<Visit>> GetVisit(int id)
         {
-            var visit = VisitStore.Visits.FirstOrDefault(v => v.Id == id);
+            var visit = await _context.Visits.FindAsync(id);
 
             if (visit == null)
             {
                 return NotFound();
             }
-            return Ok(visit);
+
+            return visit;
         }
         
         // POST: api/Visits
         [HttpPost]
-        public ActionResult<Visit> PostVisit([FromBody] Visit visit)
+        public async Task<ActionResult<Visit>> PostVisit([FromBody] Visit visit)
         {
-            var newId = VisitStore.Visits.Max(v => v.Id) + 1;
-            visit.Id = newId;
-
-            VisitStore.Visits.Add(visit);
-
+            _context.Visits.Add(visit);
+            await _context.SaveChangesAsync();
             return CreatedAtAction(nameof(GetVisit), new { id = visit.Id }, visit);
         }
         
         // PUT: api/Visits/2
         [HttpPut("{id}")]
-        public IActionResult PutVisit(int id, [FromBody] Visit visit)
+        public async Task<ActionResult> PutVisit(int id, [FromBody] Visit visit)
         {
-            var existingVisit = VisitStore.Visits.FirstOrDefault(v => v.Id == id);
-            if (existingVisit == null)
+            if (id != visit.Id)
             {
-                return NotFound();
+                return BadRequest();
             }
-            
-            existingVisit.Country = visit.Country;
-            existingVisit.City = visit.City;
-            existingVisit.YearVisited = visit.YearVisited;
+
+            _context.Entry(visit).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Visits.Any(v => v.Id == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
             
             return NoContent();
         }
         
         // DELETE: api/Visits/2
         [HttpDelete("{id}")]
-        public IActionResult DeleteVisit(int id)
+        public async Task<IActionResult> DeleteVisit(int id)
         {
-            var visit = VisitStore.Visits.FirstOrDefault(v => v.Id == id);
+            var visit = await _context.Visits.FindAsync(id);
             if (visit == null)
             {
                 return NotFound();
             }
             
-            VisitStore.Visits.Remove(visit);
+            _context.Visits.Remove(visit);
+            await _context.SaveChangesAsync();
             
             return NoContent();
         }
