7749c3d654e5c1356716047ec532c8b9402ea991
marvindeckmyn <marvindeckmyn@gmail.com>
2025-08-03 17:21:54 +0200

    Add auth0

--- travel-tracker-ui/src/app/app.component.ts
+++ travel-tracker-ui/src/app/app.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from '@auth0/auth0-angular'; // Import AuthService
 import { VisitsListComponent } from "./components/visits-list/visits-list.component";
+import { CommonModule, DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, VisitsListComponent],
+  imports: [CommonModule, RouterOutlet, VisitsListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'travel-tracker-ui';
+  constructor(@Inject(DOCUMENT) public document: Document, public auth: AuthService) {}
 }
--- travel-tracker-ui/src/app/app.config.ts
+++ travel-tracker-ui/src/app/app.config.ts
@@ -3,12 +3,21 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideAuth0, authHttpInterceptorFn } from '@auth0/auth0-angular';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient()
+    provideHttpClient(withInterceptors([authHttpInterceptorFn])),
+    provideAuth0({
+      domain: 'dev-5ryoup5zj6gd68om.us.auth0.com',
+      clientId: 'JCQ9zn7mlzRkleQQ02nk5gu99gMrIGnf',
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+        audience: 'https://api.traveltracker.com'
+      }
+    })
   ]
 };
